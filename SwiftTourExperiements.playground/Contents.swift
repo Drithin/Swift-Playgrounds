36db450e4cc1c9ea3753e44327049b8b9c9b0c63
Drithin <drithn@gmail.com>
2017-10-20 18:57:37 -0700

    Swift Tour Experiment Exercises

---
+++ SwiftTourExperiements.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//EXPERIMENT
+//Create a constant with an explicit type of Float and a value of 4
+
+let const = 4.0
+
+//Try removing the conversion to String from the last line. What error do you get?
+let label = "Width is"
+let width = 94
+let labelwidth = label + String(width)
+//Binary operator '+' cannot be applied to operands of type 'String' and 'Int' is the error without string
+
+//Use \() to include a floating-point calculation in a string and to include someone’s name in a greeting.
+
+let s1 = "Hello, you owe me \(const) and I am \(str)"
+
